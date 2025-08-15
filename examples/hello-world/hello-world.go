fc68ff60a9a36bc4b19d778b67f17adf05bf846a
Огњен Јовановић <57764299+ogi-joo@users.noreply.github.com>
2025-08-15 15:50:47 +0200

    hello-world

--- examples/hello-world/hello-world.go
+++ examples/hello-world/hello-world.go
@@ -1,5 +1,5 @@
-// Our first program will print the classic "hello world"
-// message. Here's the full source code.
+// U našem prvom programu ispisaćemo legendarnu "hello world"
+// poruku u konzoli. Ovo je ceo kod.
 package main
 
 import "fmt"
--- examples/hello-world/hello-world.sh
+++ examples/hello-world/hello-world.sh
@@ -1,17 +1,17 @@
-# To run the program, put the code in `hello-world.go` and
-# use `go run`.
+# Da bi pokrenuli program, ubacujemo kod u `hello-world.go`
+# korstimo `go run`.
 $ go run hello-world.go
 hello world
 
-# Sometimes we'll want to build our programs into
-# binaries. We can do this using `go build`.
+# Ponekad ćemo zaželeti da imamo naš program u 
+# binarnoj datoteci (npr .exe). Ovo možemo uraditi pomoću `go build`.
 $ go build hello-world.go
 $ ls
 hello-world	hello-world.go
 
-# We can then execute the built binary directly.
+# Zatim možemo pokrenuti aplikaciju direktno:
 $ ./hello-world
 hello world
 
-# Now that we can run and build basic Go programs, let's
-# learn more about the language.
+# Sada kada znamo kako ispisati i napraviti jednostavan Go program, možemo
+# da se bacimo na teže stvari.
