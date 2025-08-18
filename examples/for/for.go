15959f3b4ce222b407bb18be0a1ee4c010934c2f
Огњен Јовановић <57764299+ogi-joo@users.noreply.github.com>
2025-08-18 09:11:23 +0200

    Update for.go

--- examples/for/for.go
+++ examples/for/for.go
@@ -1,5 +1,5 @@
-// `for` is Go's only looping construct. Here are
-// some basic types of `for` loops.
+// `for` je jedina petlja u Go jeziku.
+// Ovo su neki primeri `for` petlji.
 
 package main
 
@@ -7,34 +7,35 @@ import "fmt"
 
 func main() {
 
-	// The most basic type, with a single condition.
+	// Najjednostavniji tip, sa jednim uslovom.
 	i := 1
 	for i <= 3 {
 		fmt.Println(i)
 		i = i + 1
 	}
 
-	// A classic initial/condition/after `for` loop.
+	// Klasičan `for` loop.
+	// Inicijalizacija; uslov; funkcija
 	for j := 0; j < 3; j++ {
 		fmt.Println(j)
 	}
 
-	// Another way of accomplishing the basic "do this
-	// N times" iteration is `range` over an integer.
+	// Još jedan način da se nešto "izvrši ovo N puta"
+	// je iteracijom `range` preko integer-a.
 	for i := range 3 {
 		fmt.Println("range", i)
 	}
 
-	// `for` without a condition will loop repeatedly
-	// until you `break` out of the loop or `return` from
-	// the enclosing function.
+	// `for` bez uslova će izvršavati petlju (kao while)
+	// dok se ne pozove `break` ili `return` iz
+	// unutrašnje funkcije.
 	for {
 		fmt.Println("loop")
 		break
 	}
 
-	// You can also `continue` to the next iteration of
-	// the loop.
+	// Pomoću `continue` petlja ide na sledeću
+	// iteraciju.
 	for n := range 6 {
 		if n%2 == 0 {
 			continue
