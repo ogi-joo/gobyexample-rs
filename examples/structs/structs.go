7c15dbf6269f214d8e912fa871210485930e5af9
Огњен Јовановић <57764299+ogi-joo@users.noreply.github.com>
2025-08-22 15:17:35 +0200

    Update structs.go

--- examples/structs/structs.go
+++ examples/structs/structs.go
@@ -1,23 +1,23 @@
-// Go's _structs_ are typed collections of fields.
-// They're useful for grouping data together to form
-// records.
+// Go _strukture_ su tipovane kolekcije polja.
+// Korisne su za grupisanje podataka zajedno
+// da bi napravili rekorde.
 
 package main
 
 import "fmt"
 
-// This `person` struct type has `name` and `age` fields.
+// Ova `person` struktura ima `name` i `age` polja.
 type person struct {
 	name string
 	age  int
 }
 
-// `newPerson` constructs a new person struct with the given name.
+// `newPerson` pravi novu person strukturu sa datim imenom.
 func newPerson(name string) *person {
-	// Go is a garbage collected language; you can safely
-	// return a pointer to a local variable - it will only
-	// be cleaned up by the garbage collector when there
-	// are no active references to it.
+	// Go ima ugrađeno sakupljanje smeća (garbage collecting).
+	// Možemo slobodno vratiti pokazivač na lokalnu varijablu,
+	// biće obrisan čim ne postoji ni jedna referenca koja
+	// ukazuje na njega.
 	p := person{name: name}
 	p.age = 42
 	return &p
@@ -25,38 +25,38 @@ func newPerson(name string) *person {
 
 func main() {
 
-	// This syntax creates a new struct.
+	// Ova sintaksa pravi novu strukturu.
 	fmt.Println(person{"Bob", 20})
 
-	// You can name the fields when initializing a struct.
+	// Možemo i eksplicitno pozvati polja.
 	fmt.Println(person{name: "Alice", age: 30})
 
-	// Omitted fields will be zero-valued.
+	// Zanemarena polja će imati zero-value vrednosti.
 	fmt.Println(person{name: "Fred"})
 
-	// An `&` prefix yields a pointer to the struct.
+	// Prefiks `&` vraća pokazivač na strukturu.
 	fmt.Println(&person{name: "Ann", age: 40})
 
-	// It's idiomatic to encapsulate new struct creation in constructor functions
+	// Normalno se koriste konstruktivne funkcije.
 	fmt.Println(newPerson("Jon"))
 
-	// Access struct fields with a dot.
+	// Dobijamo vrednosti polja pomoću tačke.
 	s := person{name: "Sean", age: 50}
 	fmt.Println(s.name)
 
-	// You can also use dots with struct pointers - the
-	// pointers are automatically dereferenced.
+	// Možemo koristiti tačke i sa pokazivačima na strukture,
+	// pokazivači su automatski dereferencirani.
 	sp := &s
 	fmt.Println(sp.age)
 
-	// Structs are mutable.
+	// Vrednosti polja struktura su promenjiva.
 	sp.age = 51
 	fmt.Println(sp.age)
 
-	// If a struct type is only used for a single value, we don't
-	// have to give it a name. The value can have an anonymous
-	// struct type. This technique is commonly used for
-	// [table-driven tests](testing-and-benchmarking).
+	// Ne moramo da dodeljujemo ime strukturi ako je koristimo
+	// samo za jednu varijablu. Vrednost može da ima anonimnu
+	// strukturu. Ova tehnika se često koristi za
+	// [testove](testing-and-benchmarking).
 	dog := struct {
 		name   string
 		isGood bool
