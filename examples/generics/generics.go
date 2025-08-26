75ea95a31a744164a8282a134a9a9caea218dd69
Огњен Јовановић <57764299+ogi-joo@users.noreply.github.com>
2025-08-26 12:55:10 +0200

    Update generics.go

--- examples/generics/generics.go
+++ examples/generics/generics.go
@@ -1,19 +1,19 @@
-// Starting with version 1.18, Go has added support for
-// _generics_, also known as _type parameters_.
+// Počevši od verzije 1.18, Go podržava
+// _generike_, drugačije poznati kao _tipovni parametri_.
 
 package main
 
 import "fmt"
 
-// As an example of a generic function, `SlicesIndex` takes
-// a slice of any `comparable` type and an element of that
-// type and returns the index of the first occurrence of
-// v in s, or -1 if not present. The `comparable` constraint
-// means that we can compare values of this type with the
-// `==` and `!=` operators. For a more thorough explanation
-// of this type signature, see [this blog post](https://go.dev/blog/deconstructing-type-parameters).
-// Note that this function exists in the standard library
-// as [slices.Index](https://pkg.go.dev/slices#Index).
+// Kao primer generičke funkcije, `SlicesIndex` uzima
+// slice bilo kog `comparable` tipa i element tog tipa
+// i vraća index prvog prikazivanja `v` vrednosti u `s` array-u
+// ili -1 ako nije prisutan. Ograničenje `comparable`
+// omogućava nam da vrednost ovog tipa upoređujemo sa
+// `==` i `!=` operatorima. Za detaljnije objašnjenje
+// ovog tipovnog potpisa, pogledaj [ovaj blog post](https://go.dev/blog/deconstructing-type-parameters).
+// Primetimo da se ova funkcija takođe nalazi u standardnoj
+// biblioteci - [slices.Index](https://pkg.go.dev/slices#Index).
 func SlicesIndex[S ~[]E, E comparable](s S, v E) int {
 	for i := range s {
 		if v == s[i] {
@@ -23,8 +23,8 @@ func SlicesIndex[S ~[]E, E comparable](s S, v E) int {
 	return -1
 }
 
-// As an example of a generic type, `List` is a
-// singly-linked list with values of any type.
+// Kao primer generičkog tipa, `List` je jednosmerno
+// vezana lista sa vrednostima bilo kog tipa.
 type List[T any] struct {
 	head, tail *element[T]
 }
@@ -34,9 +34,9 @@ type element[T any] struct {
 	val  T
 }
 
-// We can define methods on generic types just like we
-// do on regular types, but we have to keep the type
-// parameters in place. The type is `List[T]`, not `List`.
+// Možemo da generišemo metode sa generičkim tipovima
+// isto kao i sa regularnim... ali moramo da držimo tipovne
+// parametre robustno. Tip je `List[T]`, a ne `List`.
 func (lst *List[T]) Push(v T) {
 	if lst.tail == nil {
 		lst.head = &element[T]{val: v}
@@ -47,9 +47,9 @@ func (lst *List[T]) Push(v T) {
 	}
 }
 
-// AllElements returns all the List elements as a slice.
-// In the next example we'll see a more idiomatic way
-// of iterating over all elements of custom types.
+// AllElements vraća sve List elemente kao slice.
+// U sledećem primeru, videćemo više idiomatsko
+// iteriranje preko svih elemenata generičkog tipa.
 func (lst *List[T]) AllElements() []T {
 	var elems []T
 	for e := lst.head; e != nil; e = e.next {
@@ -61,14 +61,14 @@ func (lst *List[T]) AllElements() []T {
 func main() {
 	var s = []string{"foo", "bar", "zoo"}
 
-	// When invoking generic functions, we can often rely
-	// on _type inference_. Note that we don't have to
-	// specify the types for `S` and `E` when
-	// calling `SlicesIndex` - the compiler infers them
-	// automatically.
+	// Kada zovemo generičku funkciju, često se možemo
+	// osloniti na tipovni zaključak - _type inference_.
+	// Primetimo da ne moramo da specifiramo tipove
+	// `S`-a i `E`-a kada zovemo `SlicesIndex` - kompajler
+	// ih priziva automatski.
 	fmt.Println("index of zoo:", SlicesIndex(s, "zoo"))
 
-	// ... though we could also specify them explicitly.
+	// ... Ali ih možemo i eksplicitno prizvati.
 	_ = SlicesIndex[[]string, string](s, "zoo")
 
 	lst := List[int]{}
