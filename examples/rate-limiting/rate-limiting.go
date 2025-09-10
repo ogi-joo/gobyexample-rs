69fe2026b4e7b9c74d1e5c770e09056829788c78
Огњен Јовановић <57764299+ogi-joo@users.noreply.github.com>
2025-09-10 15:50:24 +0200

    srb: rate-limiting

--- examples/rate-limiting/rate-limiting.go
+++ examples/rate-limiting/rate-limiting.go
@@ -1,8 +1,8 @@
-// [_Rate limiting_](https://en.wikipedia.org/wiki/Rate_limiting)
-// is an important mechanism for controlling resource
-// utilization and maintaining quality of service. Go
-// elegantly supports rate limiting with goroutines,
-// channels, and [tickers](tickers).
+// [_Kontrola protoka, Rate limiting_](https://en.wikipedia.org/wiki/Rate_limiting)
+// je bitan mehanizam za kontrolisanje i upotrebu
+// resursa i održavanje kvaliteta servisa. Go
+// elegantno podržava rate limiting uz go-rutine,
+// kanale, i [ticker-e](tickers).
 
 package main
 
@@ -13,57 +13,58 @@ import (
 
 func main() {
 
-	// First we'll look at basic rate limiting. Suppose
-	// we want to limit our handling of incoming requests.
-	// We'll serve these requests off a channel of the
-	// same name.
+	// Prvo ćemo proći kroz klasičan rate limiting.
+	// Želimo da ograničimo dolazeće request-e na 200ms
+	// po request-u. Zarad primera, napravićemo i popuniti
+	// request kanal sa 5 vrednosti.
 	requests := make(chan int, 5)
 	for i := 1; i <= 5; i++ {
 		requests <- i
 	}
 	close(requests)
 
-	// This `limiter` channel will receive a value
-	// every 200 milliseconds. This is the regulator in
-	// our rate limiting scheme.
+	// Ovaj `limiter` kanal će primati vrednost
+	// svakih 200 milisekundi. On glumi regulator
+	// protoka u ovoj našoj šemi.
 	limiter := time.Tick(200 * time.Millisecond)
 
-	// By blocking on a receive from the `limiter` channel
-	// before serving each request, we limit ourselves to
-	// 1 request every 200 milliseconds.
+	// Ovde čekamo `limiter` kanal da prosledi vrednosti
+	// pre nego se nastavi sa request-om, limitiramo for petlju
+	// na 1 request svakih 200 milisekundi.
 	for req := range requests {
 		<-limiter
 		fmt.Println("request", req, time.Now())
 	}
 
-	// We may want to allow short bursts of requests in
-	// our rate limiting scheme while preserving the
-	// overall rate limit. We can accomplish this by
-	// buffering our limiter channel. This `burstyLimiter`
-	// channel will allow bursts of up to 3 events.
+	// Ponekad želimo da rešavamo kraće praskove, bursts,
+	// request-ova ali ujedno i zadržimo naš rate-limiting
+	// nakon burst-a. Ovo možemo postići baferovanjem
+	// našeg limiter kanala. Ovaj `burstyLimiter`
+	// kanal će dozvoliti burst-ove do 3 request-a.
 	burstyLimiter := make(chan time.Time, 3)
 
-	// Fill up the channel to represent allowed bursting.
+	// Naš limiter na početku dozvoljava samo 3 odjednom.
 	for range 3 {
 		burstyLimiter <- time.Now()
 	}
 
-	// Every 200 milliseconds we'll try to add a new
-	// value to `burstyLimiter`, up to its limit of 3.
+	// A onda nam svakih 200ms oslobađa mesto za jedan request.
 	go func() {
 		for t := range time.Tick(200 * time.Millisecond) {
 			burstyLimiter <- t
 		}
 	}()
 
-	// Now simulate 5 more incoming requests. The first
-	// 3 of these will benefit from the burst capability
-	// of `burstyLimiter`.
+	// Hajde sada da simuliramo 5 requesta.
 	burstyRequests := make(chan int, 5)
 	for i := 1; i <= 5; i++ {
 		burstyRequests <- i
 	}
 	close(burstyRequests)
+
+	// Prva 3 request-a će odmah proći jer limiter ima šta da
+	// vrati u njegov kanal kao signal. Poslednja dva će ići
+	// na 200ms.
 	for req := range burstyRequests {
 		<-burstyLimiter
 		fmt.Println("request", req, time.Now())
--- examples/rate-limiting/rate-limiting.sh
+++ examples/rate-limiting/rate-limiting.sh
@@ -1,5 +1,5 @@
-# Running our program we see the first batch of requests
-# handled once every ~200 milliseconds as desired.
+# Kada pokrenemo program prvo vidimo prvi skup
+# request-a, svaki rešen za oko ~200 milisekundi.
 $ go run rate-limiting.go
 request 1 2012-10-19 00:38:18.687438 +0000 UTC
 request 2 2012-10-19 00:38:18.887471 +0000 UTC
@@ -7,9 +7,9 @@ request 3 2012-10-19 00:38:19.087238 +0000 UTC
 request 4 2012-10-19 00:38:19.287338 +0000 UTC
 request 5 2012-10-19 00:38:19.487331 +0000 UTC
 
-# For the second batch of requests we serve the first
-# 3 immediately because of the burstable rate limiting,
-# then serve the remaining 2 with ~200ms delays each.
+# Za drugi skup vidimo da su prva tri izvršena skoro 
+# odmah jedan iza drugog, a potom još 2 request-a
+# sa po 200ms delay-a.
 request 1 2012-10-19 00:38:20.487578 +0000 UTC
 request 2 2012-10-19 00:38:20.487645 +0000 UTC
 request 3 2012-10-19 00:38:20.487676 +0000 UTC
